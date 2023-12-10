ceb8f2e35fd37c2d060e77bdfd43aa57b3705fd8
Achapeau <127884427+Achapeau@users.noreply.github.com>
2023-12-10 22:47:58 +0100

    add message attachment

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -10,6 +10,7 @@ import { Plus, SmileIcon } from "lucide-react";
 
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "../ui/input";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -23,6 +24,8 @@ const formSchema = z.object({
 });
 
 export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
+  const { onOpen } = useModal();
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -57,7 +60,7 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                 <div className='relative p-4 pb-6'>
                   <button
                     type='button'
-                    onClick={() => {}}
+                    onClick={() => onOpen("messageFile", { apiUrl, query })}
                     className='absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center'>
                     <Plus className='text-white dark:text-[#313338]' />
                   </button>
